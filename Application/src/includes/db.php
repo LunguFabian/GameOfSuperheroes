4841cd7d12ac5468ac58bc5a7d7dd3b3bd9c5efe
Lungu Fabian <fabian_lungu23@yahoo.com>
2025-04-25 14:23:58 +0300

    initial set-up

---
+++ Application/src/includes/db.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'db';
+$port = 3306;
+$db   = 'heroes_db';
+$user = 'user';
+$pass = 'pass';
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die("Conexiunea a eșuat: " . $conn->connect_error);
+}
+?>
---
+++ Application/src/index.php
@@ -0,0 +1,2 @@
+<?php
+echo "<h1>SuperHeroes Game is up and running!</h1>";
